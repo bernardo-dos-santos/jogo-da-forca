4b70deba7f38e965a4a8e80a14881eb2ccbe8e69
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-13 15:05:50 -0300

    Add project files.

---
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Formats.Asn1;
+
+namespace JogoDaForca.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] palavras = {
+                "ABACATE",
+                "ABACAXI",
+                "ACEROLA",
+                "ACAI",
+                "ARACA",
+                "ABACATE",
+                "BACABA",
+                "BACURI",
+                "BANANA",
+                "CAJA",
+                "CAJU",
+                "CARAMBOLA",
+                "CUPUACU",
+                "GRAVIOLA",
+                "GOIABA",
+                "JABUTICABA",
+                "JENIPAPO",
+                "MACA",
+                "MANGABA",
+                "MANGA",
+                "MARACUJA",
+                "MURICI",
+                "PEQUI",
+                "PITANGA",
+                "PITAYA",
+                "SAPOTI",
+                "TANGERINA",
+                "UMBU",
+                "UVA",
+                "UVAIA"
+            };
+
+            Random random = new Random();
+
+            int indiceEscolhido = random.Next(palavras.Length);
+
+            string palavraEscolhida = palavras[indiceEscolhido];
+
+            char[] letrasEncontradas = new char[palavraEscolhida.Length];
+
+            for(int caracter = 0; caracter < palavraEscolhida.Length; caracter++)
+            {
+                letrasEncontradas[caracter] = '_';
+            }
+
+            int Erros = 0;
+            bool jogadorEnforcou = false;
+            bool jogadorAcertou = false;
+            do
+            {
+
+                string cabecaDoBoneco = Erros >= 1 ? " o " : " ";
+                string bracoEsquerdo = Erros >= 3 ? "/" : " ";
+                string bracoDireito = Erros >= 4 ? "\\" : " ";
+                string troncoCima = Erros >= 2 ? "x" : " ";
+                string troncoBaixo = Erros >= 2 ? " x " : " ";
+                string pernas = Erros >= 5 ? "/ \\" : " ";
+
+                Console.Clear();
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Jogo da Forca");
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine(" ___________        ");
+                Console.WriteLine(" |/        |        ");
+                Console.WriteLine(" |        {0}       ", cabecaDoBoneco);
+                Console.WriteLine(" |        {0}{1}{2} ", bracoEsquerdo, troncoCima, bracoDireito);
+                Console.WriteLine(" |        {0}       ", troncoBaixo);
+                Console.WriteLine(" |        {0}       ", pernas);
+                Console.WriteLine(" |                  ");
+                Console.WriteLine(" |                  ");
+                Console.WriteLine("_|____              ");
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Erros do jogador: " + Erros);
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine("Palavra escolhida: " + String.Join("", letrasEncontradas));
+                Console.WriteLine("----------------------------------------------");
+
+                bool letraFoiEncontrada = false;
+                Console.WriteLine("Digite uma letra: ");
+                char chute = Console.ReadLine()!.ToUpper()[0];
+
+                for (int contador = 0; contador < palavraEscolhida.Length; contador++)
+                {
+                    char letraAtual = palavraEscolhida[contador];
+                    if (letraAtual == chute)
+                    {
+                        letrasEncontradas[contador] = letraAtual;
+                        letraFoiEncontrada = true;
+                    }
+
+                }
+                if (letraFoiEncontrada == false)
+                {
+                    Erros++;
+                }
+                string palavraEncontrada = string.Join("", letrasEncontradas);
+                jogadorAcertou = palavraEncontrada == palavraEscolhida;
+                jogadorEnforcou = Erros > 5;
+                if (jogadorAcertou)
+                {
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine($"Parabéns, Você ganhou o jogo !! Você Acertou a palavra {palavraEscolhida}");
+                    Console.WriteLine("----------------------------------------------");
+                } else if(jogadorEnforcou)
+                {
+                    Console.WriteLine("----------------------------------------------");
+                    Console.WriteLine("Que azar. Você não conseguiu acertar a palavra");
+                    Console.WriteLine($"A palavra era {palavraEscolhida}");
+                    Console.WriteLine("----------------------------------------------");
+                }
+                Console.ReadLine();
+
+            } while (jogadorAcertou == false && jogadorEnforcou == false);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Jogo da Forca 
+
+## Detalhes
+
+O computador irá escolher de forma aleatória uma palavra, e cabe ao usuario chutar, letra por letra, até acertar, caos ele erre mais de 5 vezes, ele perde o jogo
+
+## Entrada 
+
+Os usuários são solicitados para digitar uma letra no console, caso essa letra exista na palavra secreta, ela é mostrada no console
+
+## Funcionalidades
+**Palavra Secreta** - O computador escolhe uma palavra secreta aleatória
+
+**Desenho da forca** - A forca é desenhada progressivamente a medida dos erros do jogador
+
+**Feedback Visual** - as letras advinhadas corretamente são escritas nos seus respectivos espaços da palavra, a cada tentativa do jogador
+
+**Contagem de Erros** - O programa conta com uma contagem de erros, onde é avisado para o jogador no console quantas vezes ele errou 
+
+## Como Utilizar 
+
+1. Clone o Repositório
+´´´
