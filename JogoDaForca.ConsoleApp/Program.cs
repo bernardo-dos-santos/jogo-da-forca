b3fd0549c0d832cd7e3428779eff53cfecb9b5c4
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-18 11:50:34 -0300

    Reestruturação do Armazenamento, Criar e Ocultar Palavras

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -6,51 +6,12 @@ namespace JogoDaForca.ConsoleApp
     {
         static void Main(string[] args)
         {
-            string[] palavras = {
-                "ABACATE",
-                "ABACAXI",
-                "ACEROLA",
-                "ACAI",
-                "ARACA",
-                "ABACATE",
-                "BACABA",
-                "BACURI",
-                "BANANA",
-                "CAJA",
-                "CAJU",
-                "CARAMBOLA",
-                "CUPUACU",
-                "GRAVIOLA",
-                "GOIABA",
-                "JABUTICABA",
-                "JENIPAPO",
-                "MACA",
-                "MANGABA",
-                "MANGA",
-                "MARACUJA",
-                "MURICI",
-                "PEQUI",
-                "PITANGA",
-                "PITAYA",
-                "SAPOTI",
-                "TANGERINA",
-                "UMBU",
-                "UVA",
-                "UVAIA"
-            };
+            string[] palavras = ArmazenamentoDePalavras();
 
-            Random random = new Random();
+            string palavraEscolhida = CriarPalavra(palavras);
 
-            int indiceEscolhido = random.Next(palavras.Length);
-
-            string palavraEscolhida = palavras[indiceEscolhido];
-
-            char[] letrasEncontradas = new char[palavraEscolhida.Length];
-
-            for(int caracter = 0; caracter < palavraEscolhida.Length; caracter++)
-            {
-                letrasEncontradas[caracter] = '_';
-            }
+            char[] letrasEncontradas = OcultarPalavra(palavraEscolhida);
+            
 
             int Erros = 0;
             bool jogadorEnforcou = false;
@@ -121,5 +82,62 @@ namespace JogoDaForca.ConsoleApp
 
             } while (jogadorAcertou == false && jogadorEnforcou == false);
         }
+
+        static string[] ArmazenamentoDePalavras()
+        {
+            string[] palavras = {
+                "ABACATE",
+                "ABACAXI",
+                "ACEROLA",
+                "ACAI",
+                "ARACA",
+                "ABACATE",
+                "BACABA",
+                "BACURI",
+                "BANANA",
+                "CAJA",
+                "CAJU",
+                "CARAMBOLA",
+                "CUPUACU",
+                "GRAVIOLA",
+                "GOIABA",
+                "JABUTICABA",
+                "JENIPAPO",
+                "MACA",
+                "MANGABA",
+                "MANGA",
+                "MARACUJA",
+                "MURICI",
+                "PEQUI",
+                "PITANGA",
+                "PITAYA",
+                "SAPOTI",
+                "TANGERINA",
+                "UMBU",
+                "UVA",
+                "UVAIA"
+            };
+            return palavras;
+        }
+        static string CriarPalavra(string[] palavras)
+        {
+            Random random = new Random();
+
+            int indiceEscolhido = random.Next(palavras.Length);
+            string palavraEscolhida = palavras[indiceEscolhido];
+            return palavraEscolhida;
+        }
+
+        static char[] OcultarPalavra(string palavraEscolhida)
+        {
+            char[] letrasEncontradas = new char[palavraEscolhida.Length];
+
+            for (int caracter = 0; caracter < palavraEscolhida.Length; caracter++)
+            {
+                letrasEncontradas[caracter] = '_';
+            }
+            return letrasEncontradas;
+        }
+        
     }
 }
